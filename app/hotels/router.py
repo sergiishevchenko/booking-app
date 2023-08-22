234ce8b39f7665aa1461932848b5aed354cbacc2
Sergey Shevchenko <sergiishevchenko017@gmail.com>
2023-08-22 10:21:49 +0200

    hotel router

---
+++ app/hotels/router.py
@@ -0,0 +1,37 @@
+from datetime import date, datetime, timedelta
+from typing import List, Optional
+
+from fastapi import APIRouter, Query
+from fastapi_cache.decorator import cache
+
+from app.exceptions import CannotBookHotelForLongPeriod, DateFromCannotBeAfterDateTo
+from app.hotels.dao import HotelDAO
+from app.hotels.schemas import SHotel, SHotelInfo
+
+router = APIRouter(prefix="/hotels", tags=["Отели"])
+
+
+@router.get("/{location}")
+@cache(expire=30)
+async def get_hotels_by_location_and_time(
+    location: str,
+    date_from: date = Query(..., description=f"Например, {datetime.now().date()}"),
+    date_to: date = Query(..., description=f"Например, {(datetime.now() + timedelta(days=14)).date()}"),
+) -> List[SHotelInfo]:
+    if date_from > date_to:
+        raise DateFromCannotBeAfterDateTo
+    if (date_to - date_from).days > 31:
+        raise CannotBookHotelForLongPeriod
+    hotels = await HotelDAO.find_all(location, date_from, date_to)
+    return hotels
+
+
+@router.get("/id/{hotel_id}", include_in_schema=True)
+# This endpoint is used for the frontend when we want to display everything hotel rooms and information about the hotel itself.
+# This endpoint is responsible for information about the hotel.
+# It violates the endpoint naming rule: of course, /id/ is redundant here.
+# However, it is used because we already set the endpoint to receive hotels by their location instead of id.
+async def get_hotel_by_id(
+    hotel_id: int,
+) -> Optional[SHotel]:
+    return await HotelDAO.find_one_or_none(id=hotel_id)
